10358093c343d37a7564af9c63d0dd6b0e0b344d
Nicoly Crispi <nicoly.crispi@cifraclub.com>
2022-04-26 11:34:31 -0300

    Cria ContentView

---
+++ ssbook/UI/ViewControllers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ssbook
+//
+//  Created by Nicoly Crispi on 13/04/22.
+//
+
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        TabBarView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
