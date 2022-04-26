6fc50d599050393b4f0c819a3da857b3328bf093
Nicoly Crispi <nicoly.crispi@cifraclub.com>
2022-04-26 11:33:34 -0300

    Cria custom shape

---
+++ ssbook/UI/Shapes/RoundesRectangleCustomShape.swift
@@ -0,0 +1,21 @@
+//
+//  RoundesRectangleCustomShape.swift
+//  ssbook
+//
+//  Created by Nicoly Crispi on 14/04/22.
+//
+
+//import Foundation
+//import UIKit
+//
+//struct RoundedCornersShape: Shape {
+//    let corners: UIRectCorner
+//    let radius: CGFloat
+//    
+//    func path(in rect: CGRect) -> Path {
+//        let path = UIBezierPath(roundedRect: rect,
+//                                byRoundingCorners: corners,
+//                                cornerRadii: CGSize(width: radius, height: radius))
+//        return Path(path.cgPath)
+//    }
+//}
