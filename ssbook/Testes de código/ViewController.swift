91a65c8700e9a57813fdd5d958a33291567a7ddf
Nicoly Crispi <nicoly.crispi@cifraclub.com>
2022-04-26 11:36:42 -0300

    Tenta criar ViewController

---
+++ ssbook/Testes de código/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ssbook
+//
+//  Created by Nicoly Crispi on 20/04/22.
+//
+
+//import SwiftUI
+//
+//class ViewController: UIViewController {
+//    private var favoriteAuthorsViewModel = FavoriteAuthorsViewModel?.self
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        callToViewModelForUIUpdate()
+//    }
+//
+//    func callToViewModelForUIUpdate() {
+//        self.favoriteAuthorsViewModel = FavoriteAuthorsViewModel()
+//    }
+//}
